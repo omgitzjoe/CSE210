684cf68eae00076530c7e0b861904663434bf6f1
omgitzjoe <omgitzjoetolley@gmail.com>
2025-09-18 11:39:15 -0600

    Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What percentage did you get?:");
+        int percentage = int.Parse(Console.ReadLine());
+        string letter = "";
+        string plus_minus = "";
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter="C";
+        }
+        else if (percentage >= 60)
+        {
+            letter="D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (letter=="B"||letter=="C"||letter=="D")
+            if (percentage % 10 >= 7)
+            {
+                plus_minus = "+";
+            }
+            if (percentage%10 < 3)
+            {
+                plus_minus = "-";
+            }
+            
+        if (letter == "A" && percentage % 10 < 3)
+        {
+            plus_minus = "-";
+        }
+
+        Console.WriteLine($"You got a {letter}{plus_minus}");
+        if (percentage >= 70)
+        {
+            Console.WriteLine("C's get degrees!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, failure :(");       
+        }
     }
 }
