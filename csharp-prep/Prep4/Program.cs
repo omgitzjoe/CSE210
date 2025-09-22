00766a5b8edc53e1cf1232cac7682d7d9fde5997
omgitzjoe <omgitzjoetolley@gmail.com>
2025-09-22 16:32:01 -0600

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
-
+using System.Linq;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //initialize a list and some variables to use later
+        List<int> samples = new List<int>();
+        int number = 1;
+        double sum = 0;
+        double average = 0.0;
+        //get input
+        Console.WriteLine("Please enter integers, pressing enter for each");
+        Console.WriteLine("Enter 0 to finish");
+        while (number != 0)
+        {
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                //add to list and add its total to the sum
+                samples.Add(number);
+                sum += number;
+            }
+        }
+        //had to look this up
+        samples =samples.OrderBy(n=>n).ToList(); 
+        //give back the sorted list
+        Console.WriteLine("Here is what you entered (sorted): ");
+        foreach (int sample in samples)
+        {
+            Console.WriteLine(sample);
+        }
+        //calculations
+        Console.WriteLine($"\nThe total is {sum}");
+        average = sum / samples.Count;
+        Console.WriteLine($"The average is {average}");
+        double maxNumber = samples.Max();
+        Console.WriteLine($"The maximum is {maxNumber}");
     }
 }
