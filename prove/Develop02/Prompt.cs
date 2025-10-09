4585c6417e83726e851b39082a0872352be89a8a
omgitzjoe <omgitzjoetolley@gmail.com>
2025-10-09 17:35:15 -0600

    prompts

--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -7,7 +7,8 @@ public class Prompt
         public List<string> prompts = new List<string>
         {
         "What did you do well today?", "What are you most thankful for today?","What surprised you today?",
-        "What could you have done better today?", "What are you most proud of?"
+        "What could you have done better today?", "What are you most proud of?", "What did you do different today?",
+        "What was the highlight of the day", "How did you feel today?", "Do you regret anything today?"
         };
         public Random random = new Random();
         public string GeneratePrompt()
