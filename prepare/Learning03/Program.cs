9e97b610698c3011de4cac9f9f462bf6dbbbd743
omgitzjoe <omgitzjoetolley@gmail.com>
2025-10-14 16:05:12 -0600

    works

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction();
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
+
+        Fraction top=new Fraction(5);
+        Console.WriteLine(top.GetFractionString());
+        Console.WriteLine(top.GetDecimalValue());
+
+        Fraction both=new Fraction(3,4);
+        Console.WriteLine(both.GetFractionString());
+        Console.WriteLine(both.GetDecimalValue());
+        
+        Fraction both2=new Fraction(1,3);
+        Console.WriteLine(both2.GetFractionString());
+        Console.WriteLine(both2.GetDecimalValue());
     }
 }
